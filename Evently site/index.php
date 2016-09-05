44c7cbb56bdf90c77daa21bcedc0495a3a3db45d
byjuw <julien.wachter@gmail.com>
2016-09-05 14:17:15 +0200

    Commencement index
    Commencement index, mep bootstrap

---
+++ Evently site/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Evently</title>
+</head>
+<link rel="stylesheet" type="text/css" href="css/main.css">
+<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+
+<body>
+<?php include("menu.php"); ?>
+
+</body>
+</html>
---
+++ Evently site/menu.php
@@ -0,0 +1,10 @@
+<nav class="navbar navbar-fixed-top" role="navigation">
+    <img id="logo_blanc" src="img/logo_blanc.png">
+    <div id="menu_membre">
+    	<ul>
+    		<li>Messages</li>
+    		<li>Notifications</li>
+    		<li>Utilisateurs</li>
+    	</ul>
+    </div>
+</nav>
