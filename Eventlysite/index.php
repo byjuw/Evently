8e03165b7e8fd8945003ed3ddb3b717e8b722aab
David Lichtlé <david.lichtle@nova-beez.com>
2016-09-22 14:27:36 +0200

    Correction balises appels fichiers index.php
    Bon ben les balises de chargement de fichiers n'étaient pas dans la
    balise head.

--- Eventlysite/index.php
+++ Eventlysite/index.php
@@ -1,22 +1,24 @@
 <!DOCTYPE html>
 <html>
 <head>
+	
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+	<link rel="stylesheet" type="text/css" href="css/main.css">
+	<link rel="stylesheet" type="text/css" href="css/calendrier.css">
 	<title>Evently</title>
+	<script type="text/javascript" src="js/calendrier.js"></script>
+	<script type="text/javascript" src="js/affichemois.js"></script>
+	<script type="text/javascript" src="js/moisG.js"></script>
+	<!-- <script type="text/javascript" src="js/moisD.js"></script> -->
 </head>
-<meta charset="utf-8">
-<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
-<link rel="stylesheet" type="text/css" href="css/main.css">
-<link rel="stylesheet" type="text/css" href="css/calendrier.css">
-<script type="text/javascript" src="js/calendrier.js"></script>
-<script type="text/javascript" src="js/affichemois.js"></script>
-<script type="text/javascript" src="js/moisG.js"></script>
-<!-- <script type="text/javascript" src="js/moisD.js"></script> -->
+
 
 <body>
-<?php 
-	include("menu.php");
-	include("calendrier.php"); 
-?>
+	<?php 
+		include("menu.php");
+		include("calendrier.php"); 
+	?>
 	<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
     <!-- Include all compiled plugins (below), or include individual files as needed -->
