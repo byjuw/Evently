6d113a8d1975a2303cf5e85443a6653ced01ecfc
Meidhan68 <solide_snake68@hotmail.com>
2016-09-22 14:19:38 +0200

    Modification modal dans le calendrier pour ajout
    Modification du modal en ajoutant le formulaire pour ajouter un event

--- Eventlysite/calendrier.php
+++ Eventlysite/calendrier.php
@@ -6,22 +6,66 @@
 		</div>
 		    <script type="text/javascript">calendrier()</script>
 		<!-- Modal -->
-<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
-  <div class="modal-dialog" role="document">
-    <div class="modal-content">
-      <div class="modal-header">
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-        <h4 class="modal-title" id="myModalLabel">Modal title</h4>
-      </div>
-      <div class="modal-body">
-        ...
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Save changes</button>
+        <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+          <div class="modal-dialog" role="document">
+            <form class="modal-content">
+              <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                <h4 class="modal-title" id="myModalLabel">Création d'évènement</h4>
+              </div>
+
+              <div class="modal-body">
+            <div class="form-group">
+              <label for="name">Nom</label>
+                <input type="text" class="form-control" id="name" placeholder="Entré un nom de l'évènement">
+            </div>
+
+            <div class="form-group">
+              <label for="lieu">Lieu</label>
+                <input type="text" class="form-control" id="lieu" placeholder="Entrer un endroit">
+            </div>
+
+            <div class="form-group">
+              <label for="datetime">Heure de début / Heure de fin</label>
+                <input type="text" class="form-control" id="datetime" placeholder="Exemple: 12:42 // 16/42">
+            </div>
+
+            <div class="form-group">
+              <label for="description">Description</label>
+              <textarea name="description" class="form-control" rows="3" placeholder="Description de l'évènement"></textarea>
+            </div>
+
+              <div class="form-group">
+              <label for="organisme">Organisme organisateur</label>
+                <input type="text" class="form-control" id="organisme" placeholder="Entré un nom d'organisme-entreprise organisateur">
+            </div>
+
+            <div class="form-group">
+              <label for="co_organisateur">Co-organisateur</label>
+                <input type="text" class="form-control" id="co_organisateur" placeholder="Co-organisateur">
+            </div>
+
+            <div class="radio">
+              <label>
+                <input type="radio" name="optionsRadios" id="optionsRadios1" value="option1" checked>
+            Public
+              </label>
+            </div>
+
+            <div class="radio">
+              <label>
+                <input type="radio" name="optionsRadios" id="optionsRadios2" value="option2">
+            Privé
+              </label>
+            </div>
+
+              </div>
+              <div class="modal-footer">
+            <input name="resetform" type="reset" class="btn btn-default" value="Réinitialiser">
+            <button type="submit" class="btn btn-primary">Envoyer</button>
+              </div>
+            </form>
+          </div>
+        </div>
       </div>
     </div>
-  </div>
-</div>
-	</div>
-</div>
