053f3ee0ee78abff720a99acb92d8edf70c659ac
David Lichtlé <david.lichtle@nova-beez.com>
2016-09-22 14:51:19 +0200

    Correction menu
    Utilisation de la structure du menu responsive du bootstrap. Reste à
    corriger la version mobile du menu.

--- Eventlysite/menu.php
+++ Eventlysite/menu.php
@@ -1,10 +1,20 @@
 <nav class="navbar navbar-fixed-top" role="navigation">
-    <a href="index.php"><img id="logo_blanc" src="img/logo_blanc.png"></a>
-    <div id="menu_membre">
-    	<ul>
-    		<li>Messages</li>
-    		<li>Notifications</li>
-    		<li>Utilisateurs</li>
-    	</ul>
-    </div>
+	<div class="container-fluid">
+		<div class="navbar-header">
+			<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+				<span class="sr-only">Toggle navigation</span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+			</button>
+			<a class="navbar-brand" href="index.php"><img id="logo_blanc" src="img/logo_blanc.png"></a>
+		</div>
+		<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+		    <ul class="nav navbar-nav navbar-right">
+	    		<li><a href="#">Messages</a></li>
+	    		<li><a href="#">Notifications</a></li>
+	    		<li><a href="#">Utilisateurs</a></li>
+	    	</ul>
+	    </div>
+	</div>
 </nav>
