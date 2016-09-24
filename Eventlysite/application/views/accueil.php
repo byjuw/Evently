0e123456ef45d1f2fbf9112569ca76f15498dcf2
byjuw <julien.wachter@gmail.com>
2016-09-24 16:18:03 +0200

    Ajout des views
    Déplacement de fichiers depuis la racine vers le dossier views suite aux
    créations des controllers

---
+++ Eventlysite/application/views/accueil.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+	
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="assets/css/bootstrap.css">
+	<link rel="stylesheet" type="text/css" href="assets/css/main.css">
+	<link rel="stylesheet" type="text/css" href="assets/css/calendrier.css">
+	<title>Evently</title>
+	<script type="text/javascript" src="assets/js/calendrier.js"></script>
+</head>
+
+<body>
+	<?php 
+		include("menu.php");
+		include("calendrier.php"); 
+	?>
+	<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="assets/js/bootstrap.min.js"></script>
+</body>
+</html>
