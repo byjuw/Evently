1dd46ca6e525bcff1718754f8a8a1136416aed8b
byjuw <julien.wachter@gmail.com>
2016-09-25 10:11:14 +0200

    Organisation dossiers et fichiers
    Organisation dossiers et fichiers, suppression fichiers CI inutile et
    réglage fichier config.php en fonction des dernières modifications.

--- Eventlysite/application/config/config.php
+++ Eventlysite/application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/eventlysite';
+$config['base_url'] = 'http://localhost/evently/eventlysite';
 
 /*
 |--------------------------------------------------------------------------
@@ -438,7 +438,7 @@ $config['global_xss_filtering'] = FALSE;
 | 'csrf_regenerate' = Regenerate token on every submission
 | 'csrf_exclude_uris' = Array of URIs which ignore CSRF checks
 */
-$config['csrf_protection'] = TRUE;
+$config['csrf_protection'] = FALSE;
 $config['csrf_token_name'] = 'csrf_test_name';
 $config['csrf_cookie_name'] = 'csrf_cookie_name';
 $config['csrf_expire'] = 7200;
