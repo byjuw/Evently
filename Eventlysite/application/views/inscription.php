513948747ddc63192d16d5ecf07b45c09e3c414a
byjuw <julien.wachter@gmail.com>
2016-09-30 16:01:50 +0200

    Ajout name forms
    Ajout des names dans les formulaires.

--- Eventlysite/application/views/inscription.php
+++ Eventlysite/application/views/inscription.php
@@ -21,40 +21,40 @@
   <form method="post" action="controllers/validinscription/">
     <div class="form-group">
       <label for="nom">Nom *</label>
-      <input type="text" class="form-control" id="nom" placeholder="Nom" value="<?php echo set_value('nom'); ?>">
+      <input type="text" class="form-control" id="nom" name="nom" placeholder="Nom" value="<?php echo set_value('nom'); ?>">
       <div class="errorForm"><?php echo form_error('nom'); ?></div>
     </div>
     <div class="form-group">
       <label for="prenom">Prénom *</label>
-      <input type="name" class="form-control" id="prenom" placeholder="Prénom" value="<?php echo set_value('prenom'); ?>">
+      <input type="name" class="form-control" id="prenom" name="prenom" placeholder="Prénom" value="<?php echo set_value('prenom'); ?>">
       <div class="errorForm"><?php echo form_error('prenom'); ?></div>
     </div>
     <div class="form-group">
       <label for="email">Email *</label>
-      <input type="email" class="form-control" id="email" placeholder="Email" value="<?php echo set_value('email'); ?>">
+      <input type="email" class="form-control" id="email" name="email" placeholder="Email" value="<?php echo set_value('email'); ?>">
       <div class="errorForm"><?php echo form_error('email'); ?></div>
     </div>
     <div class="form-group">
       <label for="password">Mot de passe *</label>
-      <input type="password" class="form-control" id="password" placeholder="Mot de passe">
+      <input type="password" class="form-control" id="password" name="password" placeholder="Mot de passe">
       <div class="errorForm"><?php echo form_error('password'); ?></div>
     </div>
     <div class="form-group">
       <label for="passconf">Confirmer le mot de passe *</label>
-      <input type="password" class="form-control" id="passconf" placeholder="Confirmer le mot de passe">
+      <input type="password" class="form-control" id="passconf" name="passconf" placeholder="Confirmer le mot de passe">
       <div class="errorForm"><?php echo form_error('confpass'); ?></div>
     </div>
     <div class="form-group">
       <label for="organisme">Organisme</label>
-      <input type="text" class="form-control" id="organisme" placeholder="Entreprise ou association" value="<?php echo set_value('organisme'); ?>">
+      <input type="text" class="form-control" id="organisme" name="organisme" placeholder="Entreprise ou association" value="<?php echo set_value('organisme'); ?>">
     </div>
     <div class="form-group">
       <label for="profession">Profession</label>
-      <input type="text" class="form-control" id="profession" placeholder="Métier" value="<?php echo set_value('profession'); ?>">
+      <input type="text" class="form-control" id="profession" name="profession" placeholder="Métier" value="<?php echo set_value('profession'); ?>">
     </div>
     <div class="form-group">
       <label for="telephone">Numéro de téléphone</label>
-      <input type="text" class="form-control" id="telephone" placeholder="42 42 42 42 42" value="<?php echo set_value('telephone'); ?>">
+      <input type="text" class="form-control" id="telephone" name="telephone" placeholder="42 42 42 42 42" value="<?php echo set_value('telephone'); ?>">
     </div>
     <div id="oblig"><p>* Champs obligatoires</p></div>
     <div id="bouton">
