c387d250096a2e0adf433060b760c91cf3c5b1ad
Meidhan68 <solide_snake68@hotmail.com>
2016-11-25 10:22:05 +0100

    Connexion et deconnexion reussie

--- Eventlysite/application/views/inscription_reussie.php
+++ Eventlysite/application/views/inscription_reussie.php
@@ -1,1 +1,23 @@
-<div class="alert alert-success" role="alert">Inscription réussie</div>
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+
+	<title>Evently</title>
+
+  <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.css">
+  <link rel="stylesheet" type="text/css" href="assets/css/main.css">
+  <link rel="stylesheet" type="text/css" href="assets/css/operation_reussie.css">
+</head>
+
+<body>
+<?php 
+	include("menu.php");
+?>
+
+<div class="container" id="operation_reussie">
+	<div class="alert alert-success" role="alert">Inscription réussie</div>
+</div>
+
+</body>
+</html>
